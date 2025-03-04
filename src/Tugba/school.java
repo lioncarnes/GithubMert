5607c7b43b02896a88f1ed88b4a1dc56efc1b165
bughunter-tugba <tugbakilic3302@gmail.com>
2025-03-04 21:32:38 +0300

    sout eklendi..

--- src/Tugba/school.java
+++ src/Tugba/school.java
@@ -1,7 +1,7 @@
 package Tugba;
 
-public class School {
+public class school {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world.");
     }
 }
