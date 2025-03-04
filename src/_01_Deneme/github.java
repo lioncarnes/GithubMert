dab267e9753faecd4a49a5ea78cc71953caf07f8
lioncarnes <sdetmert@gmail.com>
2025-03-04 21:19:20 +0300

    Testing 01

---
+++ src/_01_Deneme/github.java
@@ -0,0 +1,11 @@
+package _01_Deneme;
+
+public class github {
+    public static void main(String[] args) {
+        System.out.println("Welcome To New Project");
+        // Today we are make practice for github
+
+
+
+    }
+}
