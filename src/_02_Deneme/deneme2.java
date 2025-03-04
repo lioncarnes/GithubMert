514576133df9cf742f1946024ed1beff5711fdbd
AzimKorkmaz <azimkorkmaz@gmail.com>
2025-03-04 21:38:23 +0300

    deneme2

--- src/_02_Deneme/deneme2.java
+++ src/_02_Deneme/deneme2.java
@@ -1,7 +1,7 @@
 package _02_Deneme;
 
-public class deneme {
+public class deneme2 {
     public static void main(String[] args) {
-        System.out.printf("deneme2");
+        System.out.println("deneme2");
     }
 }
